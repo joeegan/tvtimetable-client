779d708fe6740534fb1bbd15c3b475fa7756d10f
joe <joe@joeegan.co.uk>
2015-01-25 00:30:10 +0000

    updating after api url name change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(function(){
 	var containerJq = $('.container');
 	var template = $('#times-tmp').html();
 	var isLocal = !!window.location.hostname.match(/localhost/g);
-	var hostname = isLocal ? 'localhost:3000' : 'tvtimetable.herokuapp.com';
+	var hostname = isLocal ? 'localhost:3000' : 'tvtimetable-api.herokuapp.com';
 	$.getJSON('http://' + hostname + '/today').then(function(data) {
 		containerJq.append(Handlebars.compile(template)(data));
 	});
