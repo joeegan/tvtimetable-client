5f0dc8da4314f78ddf8ddd860b2e554983c6445c
joe <joe@joeegan.co.uk>
2015-01-25 15:44:28 +0000

    display episode runtime and loading data message

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function(){
-	var containerJq = $('.container');
+	var templateRenderZone = $('.tmp-zone');
 	var template = $('#times-tmp').html();
 	var isLocal = !!window.location.hostname.match(/localhost/g);
 	var hostname = isLocal ? 'localhost:3000' : 'tvtimetable-api.herokuapp.com';
 	$.getJSON('http://' + hostname + '/today').then(function(data) {
-		containerJq.append(Handlebars.compile(template)(data));
+		templateRenderZone.html(Handlebars.compile(template)(data));
 	});
 });
